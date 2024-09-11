a817bb6b8e3dea51ec35b913907695c25f5acabe
Alwyn Raj <148605730+Alwyn-411@users.noreply.github.com>
2024-09-11 17:14:43 +0530

    Update camp.sh
    Fixed New Project Structure Routes

--- camp.sh
+++ camp.sh
@@ -4,7 +4,7 @@ npm run format
 npm run build
 
 echo "common -> campus"
-cp -r /home/alwyn/Workspace-Server/CommonApp/modules/user-management-app/es5 /home/alwyn/Workspace-Server/CampusApp/node_modules/@idsk/user-management-app/
+cp -r /home/alwyn/Workspace/Frontend/CommonApp/modules/user-management-app/es5 /home/alwyn/Workspace/Frontend/CampusApp/node_modules/@idsk/user-management-app/
 
 current_time=$(date +"%H:%M:%S")
 echo "Time: $current_time"
